d3ef4424ade02cc4ac6d3964fef9e57f8c13e901
Alaa Adeen Al Kawarit <a.a.alkawarit@gmail.com>
2022-12-22 02:21:22 +0100

    creating the table- task 1 group andromeda

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
-import React from "react";
+import React from 'react';
+import './App.css';
+import Table from './table/Table';
+// import FApp from './ex2Functions/FApp.js';
+// import {AFApp}from './arrowFunctions/AFApp';
 
-function Greeting() {
-  return <h1>Hello Andromeda!</h1>
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <Table/>
+      {/* <AFApp/> */}
+        {/* <div><h1>Hii Gr44 you are best ever!!!</h1></div> */}
+     
+   
+      </header>
+    </div>
+  );
 }
 
-export default Greeting;
+export default App;
